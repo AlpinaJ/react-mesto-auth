aaba5e18cabea7cc0a49d9bd7fd3d88611570289
julia953197 <julia953197@yandex.ru>
2022-04-01 11:12:46 +0300

    Sprint 12 Added Fail popup for fail login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,17 +1,24 @@
 import React, {useState} from "react";
 import {Link, useHistory, useNavigate} from 'react-router-dom';
 import auth from "../utils/Auth.js";
+import InfoToolTip from "./InfoTooltip";
 
 function Login({handleLogin}) {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const history = useNavigate();
+    const [isPopupOpen, setPopupOpen] = useState(false);
+    const [status, setStatus] = useState();
 
     function handleSubmit(e) {
         e.preventDefault();
         auth.authorize(email, password).then((res)=>{
             if (res.token){
                 handleLogin();
+            }
+            else{
+                setStatus(false);
+                setPopupOpen(true);
             }
         }).catch((err)=>{
             console.log(err);
@@ -24,6 +31,11 @@ function Login({handleLogin}) {
     function handleSetPassword(e){
         setPassword(e.target.value);
     }
+
+    function handleClose(){
+        setPopupOpen(false);
+    }
+
     return (
         <div className="form__container">
             <h1 className="form__title">Вход</h1>
@@ -34,6 +46,7 @@ function Login({handleLogin}) {
                 </input>
                 <button type="submit" className="form__button">Войти</button>
             </form>
+            <InfoToolTip status={status} isOpen={isPopupOpen} closePopup={handleClose}/>
         </div>
     )
 }
